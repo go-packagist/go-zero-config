64e9c839a531acb244ce4d42a4a24c40292c4ef6
Flc゛ <four_leaf_clover@foxmail.com>
2023-03-10 15:10:55 +0800

    test: add test for boot

--- config_test.go
+++ config_test.go
@@ -8,9 +8,15 @@ import (
 
 func TestConfig(t *testing.T) {
 	app := foundation.NewApplication()
+	provider := NewProvider(app.Container, ".test/config.yaml")
 
-	app.Register(NewProvider(app.Container, ".test/config.yaml"))
+	app.Register(provider)
 
 	assert.Equal(t, "test.config", app.MustMake("zero.config").(*Config).Name)
 	assert.Equal(t, "127.0.0.1:8080", app.MustMake("zero.config").(*Config).ListenOn)
+	assert.Nil(t, func() error {
+		provider.Boot()
+
+		return nil
+	}())
 }
